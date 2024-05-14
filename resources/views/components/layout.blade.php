fd2ba98b091ff50803396fc46b792d45c8d224cd
Carmen De Riso <carmenderiso96@hotmail.it>
2024-05-14 21:48:29 +0200

    creata la navar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    
+    <x-navbar></x-navbar>
     {{ $slot }}
 </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,2 +1,4 @@
 <x-layout>
+
+
 </x-layout>    
