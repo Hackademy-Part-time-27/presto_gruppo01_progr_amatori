3aca804eee9eaac98d37676082658bf5c89f0789
Mattia Iacona <mattiaiacona87@icloud.com>
2024-05-15 19:36:43 +0200

    aggiunto livewire

---
+++ app/Livewire/FormAnnuncio.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class FormAnnuncio extends Component
+{
+    public function render()
+    {
+        return view('livewire.form-annuncio');
+    }
+}
