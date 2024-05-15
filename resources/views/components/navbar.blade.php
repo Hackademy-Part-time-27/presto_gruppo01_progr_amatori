ba9995f90d7a01a7b084be523281b72c1bbeaed3
Mattia Iacona <mattiaiacona87@icloud.com>
2024-05-15 21:11:16 +0200

    aggiunta tasti login,logout e register

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,18 +5,32 @@
       <h1 class="navbar-brand" style="color:rgb(230,239,230)" href="#">Presto</h1>
     </div>
     <div class="collapse navbar-collapse display-flex " style="justify-content:flex-end">
-        <button class="btn btn-outline-success" type="submit" style="background-color:rgb(230,239,230)">Accedi</button>
-        <button class="btn btn-outline-success" type="submit" style="background-color:rgb(230,239,230)">Registrati</button>
-        <button class="btn btn-outline-success" type="submit" style="background-color:rgb(230,239,230)">Inserisci annuncio</button>
+      @auth
+      <button class="btn btn-outline-success" type="submit" style="background-color:rgb(230,239,230)">
+        <a href="{{ route('announcements.create') }}">Inserisci annuncio</a>
+      </button>
         <button class="btn btn-outline-success nav-item dropdown" type="submit" style="background-color:rgb(230,239,230)">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Dropdown
             </a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Profilo</a></li>
-              <li><a class="dropdown-item" href="#">Esci</a></li>
-            </ul7>
+              <li><a class="dropdown-item" href="">{{ auth()->user()->name }}</a></li>
+              <li>
+                <form action="/logout" method="POST">
+                  @csrf
+                  <button type="submit" class="dropdown-item">Esci</button>
+                </form>
+              </li>
+            </ul>
         </button>
+      @else  
+      <button class="btn btn-outline-success" style="background-color:rgb(230,239,230)">
+        <a href="/register">Registrati</a>
+      </button>
+      <button class="btn btn-outline-success" style="background-color:rgb(230,239,230)">
+        <a href="/login">Accedi</a>
+      </button>
+      @endauth  
     </div>  
   </nav>
 </div>
