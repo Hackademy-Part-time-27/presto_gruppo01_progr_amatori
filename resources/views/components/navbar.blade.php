1e80157f133dd959703cd402f6e1f4432f42d5ee
Carmen De Riso <carmenderiso96@hotmail.it>
2024-05-15 20:05:45 +0200

    modifica navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,21 +1,22 @@
-<nav class="navbar navbar-expand-lg" style="background-color:rgb(121,183,145)" >
-  <img style="margin-left:10px" src="" alt="logo">
-  <div class="display-flex" style="text-align:center">
-    <h1 class="navbar-brand" style="color:rgb(230,239,230)" href="#">Presto</h1>
+<div>
+  <nav class="navbar navbar-expand-lg" style="background-color:rgb(121,183,145)" >
+    <img style="margin-left:10px" src="" alt="logo">
+    <div class="display-flex" style="text-align:center">
+      <h1 class="navbar-brand" style="color:rgb(230,239,230)" href="#">Presto</h1>
+    </div>
+    <div class="collapse navbar-collapse display-flex " style="justify-content:flex-end">
+        <button class="btn btn-outline-success" type="submit" style="background-color:rgb(230,239,230)">Accedi</button>
+        <button class="btn btn-outline-success" type="submit" style="background-color:rgb(230,239,230)">Registrati</button>
+        <button class="btn btn-outline-success" type="submit" style="background-color:rgb(230,239,230)">Inserisci annuncio</button>
+        <button class="btn btn-outline-success nav-item dropdown" type="submit" style="background-color:rgb(230,239,230)">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Profilo</a></li>
+              <li><a class="dropdown-item" href="#">Esci</a></li>
+            </ul7>
+        </button>
+    </div>  
+  </nav>
 </div>
-  <div class="collapse navbar-collapse display-flex " style="justify-content:flex-end">
-      <button class="btn btn-outline-success" type="submit" style="background-color:rgb(230,239,230)">Accedi</button>
-      <button class="btn btn-outline-success" type="submit" style="background-color:rgb(230,239,230)">Registrati</button>
-      <button class="btn btn-outline-success" type="submit" style="background-color:rgb(230,239,230)">Inserisci annuncio</button>
-      <button class="btn btn-outline-success nav-item dropdown" type="submit" style="background-color:rgb(230,239,230)">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Profilo</a></li>
-            <li><a class="dropdown-item" href="#">Esci</a></li>
-          </ul7>
-      </button>
-  </div>
-  
-</nav>
