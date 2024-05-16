1922f251134e33f175da5409e4f14d47f07e1a9f
Mattia Iacona <mattiaiacona87@icloud.com>
2024-05-16 21:39:10 +0200

    creata vista,controller e rotta della home con ultimi annunci

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\AnnouncementController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\AnnouncementController;
 
 /*Route::get('/', function () {
     return view('welcome');
 });*/
 
-Route::view('/','pages.index');
+Route::get('/',[FrontController::class, 'welcome'])->name('welcome');
 
 Route::get('/nuovo/annuncio', [AnnouncementController::class, 'create'])->middleware('auth')->name('announcements.create');
