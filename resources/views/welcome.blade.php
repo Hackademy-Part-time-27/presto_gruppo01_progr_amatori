6096f8be6c9731e6171d024cfba360896f3803c9
Mattia Iacona <mattiaiacona87@icloud.com>
2024-05-17 18:21:52 +0200

    modifiche frontend home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-    <div class="d-flex justify-content-center">...</div>
-        <div class="d-flex justify-content-evenly">
+    <div class="d-flex justify-content-center mt-5">...</div>
+        <div class="d-flex justify-content-evenly mb-5">
             @foreach ($categories as $category)
                 <button type="button" style="border-radius: 12px; padding: 10px 20px; border: none;
                         background-color: #79B791; color: white; cursor: pointer;">
@@ -14,7 +14,7 @@
         <div class="row">
             <div class="col-12 text-center">
                 <h1>Presto</h1>
-                <p>Ultimi annunci inseriti</p>
+                <h2>Ultimi annunci inseriti</h2>
                 <div class="row">
                     @foreach ($announcements as $announcement)
                         <div class="col-12 col-md-4 my-4">
