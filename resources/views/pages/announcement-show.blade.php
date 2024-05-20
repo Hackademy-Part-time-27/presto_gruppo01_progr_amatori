aebeb150620c6c3a29f619c00600553fac80f89b
Giovanni Ferraro <f430.gf@gmail.com>
2024-05-20 21:27:57 +0200

    rimozione button categoria in annuncio

--- resources/views/pages/announcement-show.blade.php
+++ resources/views/pages/announcement-show.blade.php
@@ -39,8 +39,7 @@
             <h5 class="card-title mt-2 mb-2">{{$announcement->title}}</h5>
             <p class="card-text">{{$announcement->description}}</p>
             <p class="card-text">{{$announcement->price}}&euro;</p>
-            <a href="{{ route('categoryShow', compact('category')) }}" class="btn mb-3" style="background-color: #79B791; color: white; cursor: pointer; 
-            width: 160px">{{$announcement->category->name}}</a>
+            <p class="card-text">{{$announcement->category->name}}</p>
              <p class="card-footer">Pubblicato il: {{$announcement->created_at->format('d/m/Y')}} <br>
              Autore: {{$announcement->user->name ?? ''}}</p>
     
