71ddf426886a0cd19800130b8dd134fb796394b0
Mattia Iacona <mattiaiacona87@icloud.com>
2024-06-03 20:38:55 +0200

    implementato job safesearch

--- app/Livewire/FormAnnuncio.php
+++ app/Livewire/FormAnnuncio.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 use App\Models\Category;
 use App\Jobs\ResizeImage;
 use App\Models\Announcement;
+use App\Jobs\GoogleVisionSafeSearch;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Livewire\Features\SupportFileUploads\WithFileUploads;
@@ -76,6 +77,7 @@ class FormAnnuncio extends Component
                 $newImage = $this->announcement->images()->create(['path'=>$image->store($newFileName, 'public')]);
 
                 dispatch(new ResizeImage($newImage->path, 400, 300));
+                dispatch(new GoogleVisionSafeSearch($newImage->id));
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
