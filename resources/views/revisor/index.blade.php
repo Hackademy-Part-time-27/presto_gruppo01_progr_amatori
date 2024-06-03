472251121fa34c21f49f9a13776e53fdbcf7e860
Giovanni Ferraro <f430.gf@gmail.com>
2024-06-03 21:41:20 +0200

    implementazione vista safesearch (da sistemare layout)

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -19,6 +19,26 @@
                             <div class="carousel-item @if($loop->first)active @endif">
                                 <img src="{{$image->getUrl(400, 300)}}" alt="" class="img-fluid p-3 rounded">
                             </div>
+                            <div class="col-md-3 border-end">
+                                <h5 class="tc-accent mt-3">Tags</h5>
+                                    <div class="p-2">
+                                        @if ($image->labels)
+                                            @foreach ($image->labels as $label)
+                                                <p class="d-inline">{{$label}}</p>
+                                            @endforeach
+                                        @endif        
+                                    </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="card-body">
+                                    <h5 class="tc-accent">Revisione Immagini</h5>
+                                    <p>Adulti: <span class="{{$image->adult}}"></span></p>
+                                    <p>Satira: <span class="{{$image->spoof}}"></span></p>
+                                    <p>Medicina: <span class="{{$image->medical}}"></span></p>
+                                    <p>Violenza: <span class="{{$image->violence}}"></span></p>
+                                    <p>Insinuante: <span class="{{$image->racy}}"></span></p>
+                                </div>
+                            </div>          
                         @endforeach
                     </div>
                 @else                   
