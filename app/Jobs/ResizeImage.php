977acb4e7d89db63e3f2693f24593c1ffe4e1d61
Giovanni Ferraro <f430.gf@gmail.com>
2024-06-04 21:58:55 +0200

    inserimento watermark(da ridimensionare)

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -3,6 +3,8 @@
 namespace App\Jobs;
 
 use Spatie\Image\Image;
+use Spatie\Image\Enums\Fit;
+use Spatie\Image\Enums\Unit;
 use Illuminate\Bus\Queueable;
 use Spatie\Image\Enums\CropPosition;
 use Illuminate\Queue\SerializesModels;
@@ -41,6 +43,8 @@ class ResizeImage implements ShouldQueue
 
         $croppedImage = Image::load($scrPath)
                         ->crop($w, $h, CropPosition::Center)
+                        ->watermark('public/img/logopresto.png',alpha: 80, width:15,widthUnit:Unit::Percent,
+                        height:9,heightUnit:Unit::Percent, fit: Fit::Stretch)
                         ->save($destPath);
     }
 }
