4983cfdfc518aab20937133fa3594264354bccd0
Giovanni Ferraro <f430.gf@gmail.com>
2024-06-04 20:14:07 +0200

    implementazione censura volti

--- app/Livewire/FormAnnuncio.php
+++ app/Livewire/FormAnnuncio.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use App\Models\Announcement;
 use App\Jobs\GoogleVisionLabelImage;
@@ -78,9 +79,11 @@ class FormAnnuncio extends Component
                 $newFileName = "announcements/{$this->announcement->id}";
                 $newImage = $this->announcement->images()->create(['path'=>$image->store($newFileName, 'public')]);
 
-                dispatch(new ResizeImage($newImage->path, 400, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 400, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id),
+                ])->dispatch($newImage->id);    
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
